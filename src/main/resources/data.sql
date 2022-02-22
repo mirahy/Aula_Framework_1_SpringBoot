a627285deaccc99491073be749213b2c53931c37
MIRAHY RIBEIRO BRANCO FONSECA <02457680174@ifms.edu.br>
2022-02-22 19:19:48 -0400

    add arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_tecnico (nome, cpf, telefone, email, endereco, arbito)
+VALUES ('Pedro Calvacante', '123.456.789-00', '67 88888-9999', 'pedro@gmail.com', 'Rua dois, 137', 1),
+('Jose Silva', '123.456.789-00', '67 88888-9999', 'jose.silva@gmail.com', 'Rua dois, 137', 0);
